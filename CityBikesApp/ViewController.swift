0e78263bde987caafe2356b56515230466c7d1e6
Kristoffer Anger <kristoffer.anger@gmail.com>
2019-03-06 08:58:42 +0100

    Initial Commit

---
+++ CityBikesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityBikesApp
+//
+//  Created by Kristoffer Anger on 2019-03-06.
+//  Copyright © 2019 kriang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
